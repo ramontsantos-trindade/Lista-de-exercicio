bd40f87bce4670faad9b7c2735181ed5c7cd143d
ramontsantos-trindade <ramon_t_santos@estudante.sesisenai.org.br>
2026-07-02 08:28:35 -0300

    add - finalizado

---
+++ ex_01.php
@@ -0,0 +1,22 @@
+<?php
+
+// (x² + y²) ? (x+y)
+
+?>
+
+function calcularformula($x, $y) 
+{
+    if ($x + $y == 0) {         
+   return "Não é possivel dividir por zero"; }
+
+   $resultado = ((pow($x, 2) + pow($y, 2)) / ($x + $y));
+
+   return $resultado;
+
+}
+$x = 10;
+$y = 5
+
+echo "Valor de x: " . $x . "<br>";
+echo "Valor de y: " . $y . "<br><br>";
+echo "Resultado da fórmula: " . calcularformula($x, $y);
