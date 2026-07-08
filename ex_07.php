23989235faff32cd735a9568812a455ebffbab06
ramontsantos-trindade <ramon_t_santos@estudante.sesisenai.org.br>
2026-07-08 09:32:30 -0300

    ex 07

--- ex_07.php
+++ ex_07.php
@@ -1,1 +1,15 @@
 <?php
+function calcularDesconto(){
+    if ($valortotal > 1000) {
+        $desconto = 0.30;
+    } elseif ($valortotal > 500) {
+        $desconto = 0.20;
+    } elseif ($valortotal > 100) {
+        $desconto = 0.10;
+    } else {
+        $desconto = 0;
+    }
+    return $valortotal * $desconto;
+
+    echo "O valor do desconto é: " . calcularDesconto();
+}
