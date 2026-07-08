dc850ba7dd750f058e811934cecc34c0e9ae0a89
ramontsantos-trindade <ramon_t_santos@estudante.sesisenai.org.br>
2026-07-08 09:45:58 -0300

    metade da 8

---
+++ ex_08.php
@@ -0,0 +1,6 @@
+<?php
+function ordenarNomes(){
+    $nomes = array("João", "Maria", "Pedro", "Ana", "HOMOERECTUS");
+    sort($nomes);
+    return $nomes;
+}                                               
