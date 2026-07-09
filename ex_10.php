3b1bd9933ee1e1df1b515843c868c115a25846c7
ramontsantos-trindade <ramon_t_santos@estudante.sesisenai.org.br>
2026-07-09 08:41:48 -0300

    add - ex 10

--- ex_10.php
+++ ex_10.php
@@ -1,6 +1,34 @@
 <?php
 
-function calcularMedia(){
+function calcularMedia(array $notas) {
     
 
+    $maiorNota = max($notas);
+    $menorNota = min($notas);
+
+   
+    $media = array_sum($notas) / count($notas);
+
+    if ($media >= 7) {
+        $resultado = "Aprovado";
+    } else {
+        $resultado = "Reprovado";
+    }
+
+
+    return [
+        'maiorNota' => $maiorNota,
+        'menorNota' => $menorNota,
+        'media' => $media,
+        'situacao' => $resultado
+    ];
 }
+
+$notas = array(7, 8, 9, 6, 10);
+
+$resultado = calcularMedia($notas);
+
+ echo "Maior nota: " . $resultado['maiorNota'] . "<br>";
+echo "Menor nota: " . $resultado['menorNota'] . "<br>";
+echo "Média: " . $resultado['media'] . "<br>";
+echo "Situação: " . $resultado['situacao'] . "<br>";
