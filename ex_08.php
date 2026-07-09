49fc2469ee3ead38bbe4145d95a32b8d05911f9f
ramontsantos-trindade <ramon_t_santos@estudante.sesisenai.org.br>
2026-07-09 07:45:29 -0300

    add - terminei a 08

--- ex_08.php
+++ ex_08.php
@@ -3,5 +3,11 @@ function ordenarNomes(){
     $nomes = array("João", "Maria", "Pedro", "Ana", "HOMOERECTUS");
     sort($nomes);
     return $nomes;
+
+    print_r($nomes);
     
 }                                               
+
+echo "Nomes ordenados: <br>";
+$nomesOrdenados = ordenarNomes($nomes);
+print_r($nomesOrdenados);
