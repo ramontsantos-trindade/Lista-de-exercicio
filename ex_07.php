8cc8657f24e769a6c2a936278645158fbde6b392
ramontsantos-trindade <ramon_t_santos@estudante.sesisenai.org.br>
2026-07-09 07:29:52 -0300

    07 finalizada

--- ex_07.php
+++ ex_07.php
@@ -1,1 +1,26 @@
 <?php
+
+function calcularDesconto($valor){
+
+if ($valor < 500){
+    return "Sem Desconto";
+}
+
+else if ($valor < 500){
+    return $valor - ($valor * 0.01); 
+}
+
+else if ($valor < 1000){
+    return $valor - ($valor * 0.02); 
+}
+
+else if ($valor < 1000){
+    return $valor - ($valor * 0.3); 
+}
+
+}
+
+$valor = 550;
+
+echo "Valor original: " . $valor . "<br>";
+echo "Valor com desconto: " . calcularDesconto($valor);
