186d9eecf8b68943ca0e9f2c93df1bc22e0c8382
ramontsantos-trindade <ramon_t_santos@estudante.sesisenai.org.br>
2026-07-09 08:14:43 -0300

    add - ex 09

--- ex_09.php
+++ ex_09.php
@@ -1,3 +1,30 @@
 <?php
 
-function analisarNumero()
+function analisarNumero($numero){
+    
+    $parOuimpar = ($numero % 2 == 0) ? "Par" : "Ímpar";
+
+    $primo = true;
+    if ($numero <= 1) {
+        $primo = false;
+    } else {
+        for ($i = 2; $i <= sqrt($numero); $i++) {
+            if ($numero % $i == 0) {
+                $primo = false;
+                break;
+            }
+        }
+    }
+
+    $numeroPrimo = $primo ? "É primo" : "Não é primo";
+
+    $somaDivisores = 0;
+    for ($i = 1; $i <= $numero; $i++) {
+        if ($numero % $i == 0) {
+            $somaDivisores += $i;
+        }
+    }
+    $numeroPerfeito = ($somaDivisores == $numero) ? "É perfeito" : "Não é perfeito";
+
+    return [$parOuimpar, $numeroPrimo, $numeroPerfeito];
+}
